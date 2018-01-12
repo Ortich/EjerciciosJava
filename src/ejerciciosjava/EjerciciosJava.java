92a032f0070f536450bf1b3468d65019ccc2cf17
Ortich <d.ortich.oficial@gmail.com>
2018-01-12 17:28:33 +0100

    Ejemplos de Strings como cadenas

--- src/ejerciciosjava/EjerciciosJava.java
+++ src/ejerciciosjava/EjerciciosJava.java
@@ -28,14 +28,40 @@ public class EjerciciosJava {
 	return numerosMaximos;
     }
     
+    public void ejemplosString(){
+	String cadena1 = "Acaso hubo buhos aca";
+	//Para buscar un eslavon concreto de la cadena
+	System.out.println(cadena1.charAt(7));
+	System.out.println(cadena1.charAt(0));
+	System.out.println(cadena1.charAt(cadena1.length()-1));
+	//Para buscar una ristra de cadenas en concreto
+	System.out.println(cadena1.substring(6, 9));
+	System.out.println(cadena1.substring(11, 16));
+	System.out.println(cadena1.substring(11,cadena1.length()));
+	System.out.println(cadena1.substring(6));
+	//Array por palabras, dado que lo que separan las palabras es un espacio,
+	//el split lo marcara el " "(Espacio en blanco)
+	String[] arrayPalabras = cadena1.split(" ");
+	System.out.println(arrayPalabras[2]);
+	System.out.println(arrayPalabras[0]);
+	//Funciones de busqueda 
+	System.out.println(cadena1.indexOf('h')); //Posicion de la primera aparicion de la h
+	System.out.println(cadena1.indexOf('z'));//si no encuentra la letra pone -1
+	System.out.println(cadena1.indexOf("buho"));//Busca tambien por palabras
+	System.out.println(cadena1.indexOf("culo"));//tambien puede buscar palabras que no estan y sale -1
+    }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 	EjerciciosJava ejercicio1 = new EjerciciosJava();
-	int [] listaNumeros = {150, 31,27, 2, 5, 99};
-	System.out.println(Arrays.toString(ejercicio1.maximo(listaNumeros)));
+	//Pruebas para maximo
+//	int [] listaNumeros = {150, 31,27, 2, 5, 99};
+//	System.out.println(Arrays.toString(ejercicio1.maximo(listaNumeros)));
+
+	//Pruebas para ejemplosString
+	ejercicio1.ejemplosString();
 	
 	
     }
