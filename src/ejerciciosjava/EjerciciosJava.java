354009b44b100c7867f1ee20f8110f239c7a610e
Ortich <d.ortich.oficial@gmail.com>
2018-01-15 20:31:02 +0100

    Palindromo iniciado. Borra espacios, no se si funciona con puntos y comas.

--- src/ejerciciosjava/EjerciciosJava.java
+++ src/ejerciciosjava/EjerciciosJava.java
@@ -14,11 +14,10 @@ public class EjerciciosJava {
     
     public int[] maximo(int[] listaNumeros){
 	int[] numerosMaximos = new int[2];
+	//Puede tambien iniciarse de la siguiente manera: int [] numerosMaximos = {0,0};
 	for(int i = 0; i<listaNumeros.length;i++){
 	    if(numerosMaximos[0] < listaNumeros[i]){
-		if(numerosMaximos[0]>numerosMaximos[1]){
-		    numerosMaximos[1]= numerosMaximos[0];
-		}
+		numerosMaximos[1]= numerosMaximos[0];
 		numerosMaximos[0] = listaNumeros[i]; 
 	    }
 	    else if(numerosMaximos[1] < listaNumeros[i]){
@@ -26,6 +25,17 @@ public class EjerciciosJava {
 	    }
 	}
 	return numerosMaximos;
+    }
+    
+    public boolean palindromo(String frase){
+    boolean palindromo = true;
+    String fraseSimple= frase.replaceAll("\\W","");
+    for(int i = 0; i < fraseSimple.length();i++){
+	if(fraseSimple.charAt(i)!=fraseSimple.charAt(fraseSimple.length()-i-1)&& palindromo==true){
+	    palindromo = false;
+	}
+    }
+    return palindromo;
     }
     
     public void ejemplosString(){
@@ -57,12 +67,14 @@ public class EjerciciosJava {
     public static void main(String[] args) {
 	EjerciciosJava ejercicio1 = new EjerciciosJava();
 	//Pruebas para maximo
-//	int [] listaNumeros = {150, 31,27, 2, 5, 99};
-//	System.out.println(Arrays.toString(ejercicio1.maximo(listaNumeros)));
+	int [] listaNumeros = {150, 31,27, 2, 5, 99};
+	System.out.println(Arrays.toString(ejercicio1.maximo(listaNumeros)));
 
 	//Pruebas para ejemplosString
 	ejercicio1.ejemplosString();
 	
+	//Pruebas para palindromo
+	System.out.println(ejercicio1.palindromo("acaso hubo buhos aca"));
 	
     }
     
